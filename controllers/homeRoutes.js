4b602a0aafac275701bd1305bf4a6aa5504be1e4
Miles Thomas <milesman2013@gmail.com>
2022-12-03 13:11:40 +1100

    finished home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,26 @@
 const router = require('express').Router();
-const withAuth = require('../utils/auth')
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-    res.render('homepage')
+    res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login');
 });
 
 router.get('/signup', (req, res) => {
-    res.render('signup')
+    res.render('signup');
+});
+
+router.get('/createpost', withAuth, (req, res) => {
+    res.render('createpost');
+});
+
+router.get('/postlist', withAuth, (req, res) => {
+    res.render('posts');
+});
+
+router.get('/viewpost', withAuth, (req, res) => {
+    res.render('viewpost');
 });
